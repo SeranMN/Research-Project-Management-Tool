7061f55b5c459049086ff50b79832caa5a8a9919
SeranMN <88319801+SeranMN@users.noreply.github.com>
2022-05-21 21:36:53 +0530

    initial commit

---
+++ BACKEND/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require('cors');
+const bodyParser = require("body-parser");
+const dotenv = require('dotenv');
+const connectDB = require("./src/config/config");
+
+const app = express();
+
+const PORT = process.env.PORT || 5001;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+connectDB();
+
+
+app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`);
+  });
+
