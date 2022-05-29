82a70d42ea50a5c885ba4485f11fa6f6ce752b01
navod99 <navx99@gmail.com>
2022-05-29 09:37:18 +0530

    created Admin Dashboard

--- fRONTEND/Routing.js
+++ fRONTEND/Routing.js
@@ -1,16 +1,23 @@
 
 import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Login from './Components/user/Login'
-import CreateAccount from './Components/user/CreateAccount'
-import Profile from './Components/user/profile'
+import AdminDashboard from './Components/Admin/AdminDashboard';
+import PanelDashboard from './Components/PanelMember/PanelDashboard';
+// import Login from './Components/user/Login'
+// import CreateAccount from './Components/user/CreateAccount'
+// import Profile from './Components/user/profile'
+
 const Routing = () => {
+
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' exact element={<Login />} />
+                {/* <Route path='/' exact element={<Login />} />
                 <Route path='/signin' exact element={<CreateAccount />} />
-                <Route path = '/profile' exact element = {<Profile/>}/>
+                <Route path = '/profile' exact element = {<Profile/>}/> */}
+                <Route path='/admindashboard' exact element={<AdminDashboard/>}/>
+                <Route path='/paneldashboard' exact element={<PanelDashboard/>}/>
+                
             </Routes>
         </BrowserRouter>
   );
