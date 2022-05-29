b2a84b27bd438d04db826eba6cc9fcedfcce6e1c
SeranMN <88319801+SeranMN@users.noreply.github.com>
2022-05-30 01:43:40 +0530

    modified login

--- fRONTEND/Components/user/Login.js
+++ fRONTEND/Components/user/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Grid from "@mui/material/Grid";
-import { Card, TextField, Typography } from "@mui/material";
+import { Card, TextField, ThemeProvider, Typography,createTheme,Box,Paper } from "@mui/material";
 import Stack from "@mui/material/Stack";
 import Masonry from "@mui/lab/Masonry";
 import Button from "@mui/material/Button";
@@ -9,8 +9,20 @@ import CardMedia from "@mui/material/CardMedia";
 import { Link } from "react-router-dom";
 import axios from 'axios'
 import { useNavigate } from "react-router-dom";
+import CssBaseline from '@mui/material/CssBaseline';
+import Snackbar from '@mui/material/Snackbar'
+import { forwardRef } from "react";
+import MuiAlert from '@mui/material/Alert';
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
 const Login = () => {
   const navigate = useNavigate();
+
+  const [open, setOpen] = React.useState(false);
+  const theme = createTheme();
   const [regNo, setRegNo] = useState('')
   const [pwd, setpwd] = useState('')
   const login = () => {
@@ -21,26 +33,56 @@ const Login = () => {
     axios.post('http://localhost:5001/login/login', det)
       .then((res) => {
         if (res.data != "") {
-          alert('sucessfully loged in')
+         
           sessionStorage.setItem("user", det.regNo)
           console.log(det.regNo)
           navigate(`/profile/${res.data}`)
           
         } else {
-          alert('At least one credintial is wrong')
+         setOpen(true)
       }
     })
   }
+
+  
+
+   const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
   return (
-    
-    <div style={{
-      backgroundImage: `url("https://www.intotheminds.com/blog/app/uploads/desk-research-group-tn.jpg")`,
-    }}
-   >
+    <ThemeProvider theme={theme}>
+    <Grid container component="main" sx={{ height: '100vh' }}>
+                <CssBaseline />
+                <Grid
+                    item
+                    xs={false}
+                    sm={6}
+                    md={9}
+                    sx={{
+                        backgroundImage: `url(https://www.intotheminds.com/blog/app/uploads/desk-research-group-tn.jpg)`,
+                        backgroundRepeat: 'no-repeat',
+                        backgroundColor: (t) =>
+                            t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                    }}
+                />
       
      
-          <div
-            style={{ position: "fixed", bottom: "45%", right: 5, width: "33%" }}>
+          <Grid item xs={12} sm={8} md={3} component={Paper} elevation={6} square>
+                    <Box
+                        sx={{
+                            my: 20,
+                            mx: 6,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
             <Stack spacing={2}>
               <TextField id="filled-basic" label="User Name" variant="filled" value={regNo} onChange={(e)=>setRegNo(e.target.value) }/>
               <TextField
@@ -50,19 +92,32 @@ const Login = () => {
                 type="password"
                 value={pwd}
                 onChange={(e) => setpwd(e.target.value)}
+                
               />
               <Button variant="contained" color="success" onClick={login}>
                 Login
               </Button>
-              <Link to="/signin" >
-                {" "}
+              <Link to="/signin" variant="body2" >
+               
                 <Typography>Create Account</Typography>
               </Link>
             </Stack>
-          </div>
+          </Box>
+          </Grid>
         
         
-    </div>
+      </Grid>
+       <Snackbar
+        anchorOrigin={{vertical: 'top',horizontal:'center' }}
+        open={open}
+        onClose={handleClose}
+        autoHideDuration={1000}
+        key={'top' + 'center'}
+      >
+
+        <Alert severity="error"  onClose={handleClose} >Wrong credintials !</Alert>
+      </Snackbar>
+      </ThemeProvider>
   );
 };
 
