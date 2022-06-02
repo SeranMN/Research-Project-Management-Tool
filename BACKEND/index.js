970cfddbaabd71e79549637977be2b24495e06b3
Ashen Hettiarachchi <ashenirhettiarachchi@gmail.com>
2022-06-02 17:29:59 +0530

    implemented submission backend

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -3,15 +3,19 @@ const cors = require('cors');
 const bodyParser = require("body-parser");
 const dotenv = require('dotenv');
 const connectDB = require("./src/config/config");
+const SubmissionTypeRouter = require("./src/Route/submissionType-routes");
 
 const app = express();
 
 const PORT = process.env.PORT || 5001;
 
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
 app.use(bodyParser.json());
 
 connectDB();
+
+app.use("/submissionType", SubmissionTypeRouter());
 
 
 app.listen(PORT, () => {
---
+++ BACKEND/src/Route/submissionType-routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const SubmissionTypeController = require('../controller/submissionType-controller');
+
+module.exports = function (){
+    router.post('/create', SubmissionTypeController.addSubmissionType);
+    router.get('/', SubmissionTypeController.getAllSubmissionTypes);
+    router.put('/update/:id', SubmissionTypeController.updateSubmissionType);
+    router.delete('/delete/:id', SubmissionTypeController.deleteSubmissionType);
+    return router;
+}
